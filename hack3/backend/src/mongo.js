e97eff212088eacb9059a6b3ccad0e4f6412085f
Beer-and-Skittles <jessicahu1012@gmail.com>
2022-12-20 10:26:09 +0800

    first half done

---
+++ hack3/backend/src/mongo.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import { dataInit } from "./upload.js";
+
+import "dotenv-defaults/config.js";
+
+mongoose.set("strictQuery", true);
+
+async function connect() {
+  // TODO 1 Connect to your MongoDB and call dataInit()
+  // config();
+        if (!process.env.MONGO_URL) {
+            console.error("Missing MONGO_URL!!!");
+            process.exit(1);
+        }    
+        mongoose.connect(process.env.MONGO_URL, {
+            useNewURLParser: true,
+            useUnifiedTopology: true,
+        })
+        .then((res) => console.log('mongo db connection created'));
+        mongoose.connection.on('error',
+        console.error.bind(console, 'connection error:'));
+  // TODO 1 End
+}
+
+export default { connect };
